4b9d7c7f45ea31f2e503df8d972b56e455e53ba9
abdulazizraxmonov <raxmonovabdulaziz81@gmail.com>
2024-03-07 14:57:41 +1300

    first commit

---
+++ project/urls.py
@@ -0,0 +1,29 @@
+"""
+URL configuration for project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+# urls.py
+
+from django.urls import path
+from my_app.views import page1, page2, page3, page4, page5
+
+urlpatterns = [
+    path('', page1, name='home'),  # Добавляем обработчик для пустого пути
+    path('page1/', page1, name='page1'),
+    path('page2/', page2, name='page2'),
+    path('page3/', page3, name='page3'),
+    path('page4/', page4, name='page4'),
+    path('page5/', page5, name='page5'),
+]
